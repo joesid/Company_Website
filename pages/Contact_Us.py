d87df4fcac97f65b22ce90bbea01d41e890260c6
joesid <joesidious@gmail.com>
2023-07-23 11:18:28 +0100

    Implement "Select Option" for email msg

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,18 +1,22 @@
 import streamlit as st
 from send_email import send_email
+import pandas as pd
 
 st.header("Contact Me")
 
-options = ["Option 1", "Option 2"]
+df = pd.read_csv('topics.csv', sep=',')
+topic = df['topic'].tolist() #read content of column 'topic'
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email address")
-    selected_option = st.selectbox("What topic do you want to discuss:", options)
+    selected_option = st.selectbox("What topic do you want to discuss:", topic)
     raw_message = st.text_area("Your message")
     message = f"""\
 Subject: New email from {user_email}
 
+Topic: {selected_option}
 From: {user_email}
+
 {raw_message}
 """
     button = st.form_submit_button('Submit')
